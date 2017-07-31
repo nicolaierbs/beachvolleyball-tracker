81737b7fbb259ded8eafad63b01f575c738160be
Nicolai Erbs <nico.erbs@gmail.com>
2017-07-31 15:41:52 +0200

    Initial Commit

---
+++ beachvolleyballTests/beachvolleyballTests.swift
@@ -0,0 +1,36 @@
+//
+//  beachvolleyballTests.swift
+//  beachvolleyballTests
+//
+//  Created by Erbs on 31.07.17.
+//  Copyright © 2017 Nicolai Erbs. All rights reserved.
+//
+
+import XCTest
+@testable import beachvolleyball
+
+class beachvolleyballTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
