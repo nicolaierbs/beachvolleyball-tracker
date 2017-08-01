2dc8880c90e0c3bbdfe04d622c47bdff2c6f13f5
Nicolai Erbs <nico.erbs@gmail.com>
2017-08-01 02:01:42 +0200

    Initial import

--- beachvolleyball WatchKit Extension/InterfaceController.swift
+++ beachvolleyball WatchKit Extension/InterfaceController.swift
@@ -8,10 +8,15 @@
 
 import WatchKit
 import Foundation
+import CoreMotion
 
 
 class InterfaceController: WKInterfaceController {
-
+    
+    @IBOutlet var detectedAction: WKInterfaceLabel!
+    
+    var motionManager: CMMotionManager!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
@@ -21,6 +26,20 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        let queue = OperationQueue()
+        
+        motionManager = CMMotionManager()
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.deviceMotionUpdateInterval = 1
+            motionManager.startDeviceMotionUpdates(to: queue) {
+                [weak self] (data: CMDeviceMotion?, error: Error?) in
+                if let gravity = data?.gravity {
+                    let rotation = atan2(gravity.x, gravity.y) - Double.pi
+                    print (rotation)
+                }
+            }
+        }
     }
     
     override func didDeactivate() {
@@ -28,4 +47,13 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
-}
+    @IBAction func addActionLowerReception() {
+        print("Add action: lower reception")
+        detectedAction.setText("Untere Annahme")
+    }
+    @IBAction func addActionUpperReception() {
+        print("Add action: upper reception")
+        detectedAction.setText("Obere Annahme")
+    }
+    
+            }
